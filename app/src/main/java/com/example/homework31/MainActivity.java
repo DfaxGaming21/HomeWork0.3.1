0c93059bc0486347daef716db80af608fc3c493d
DfaxGaming21 <124247717+DfaxGaming21@users.noreply.github.com>
2023-04-02 01:30:29 +0700

    HomeWork0.3.1
    HomeWork0.3.1

---
+++ app/src/main/java/com/example/homework31/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.homework31;
+import static androidx.core.content.PackageManagerCompat.LOG_TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private TextView textCount;
+    private Button button;
+    private int count = 0;
+    int cnt = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        textCount = findViewById(R.id.textCount);
+        button = findViewById(R.id.button);
+
+
+        button.setOnClickListener(listener);
+    }
+
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            count++;
+            textCount.setText(Integer.toString(count+cnt));
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        Toast toast = Toast.makeText(this, "Старт активности", Toast.LENGTH_SHORT); //
+        toast.show();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        Toast toast = Toast.makeText(this, "Стоп активности", Toast.LENGTH_SHORT); // инициализация
+        toast.setGravity(Gravity.LEFT, 0,0);
+        toast.show(); //
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Toast toast = Toast.makeText(this, "Уничтожение активности", Toast.LENGTH_SHORT); // инициализация
+        toast.setGravity(Gravity.BOTTOM, 0,0);
+        toast.show();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        Toast toast = Toast.makeText(this, R.string.pause_activity, Toast.LENGTH_SHORT); // инициализация
+        toast.setGravity(Gravity.TOP, 0,0);
+        toast.show();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+
+        Toast toast = Toast.makeText(this, R.string.resume_activity, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0,0);
+        LinearLayout toastContainer = (LinearLayout) toast.getView();
+        ImageView cat = new ImageView(this);
+        cat.setImageResource(R.drawable.cat);
+        toastContainer.addView(cat, 1);
+        toast.show();
+        super.onResume();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("cnt",count);
+        Log.d(LOG_TAG, "onSaveInstanceState");}
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        count = savedInstanceState.getInt("cnt");
+        Log.d(LOG_TAG, "onRestoreInstanceState");
+    }
+
+}
